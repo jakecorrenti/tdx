70c8acc24d14a2cebf53c8da1bcc633f193a7adb
Jake Correnti <jakecorrenti+github@proton.me>
2024-03-21 11:40:51 -0400

    start of work
    Signed-off-by: Jake Correnti <jakecorrenti+github@proton.me>

--- src/vcpu/linux/ioctl.rs
+++ src/vcpu/linux/ioctl.rs
@@ -1,1 +1,30 @@
 // SPDX-License-Identifier: Apache-2.0
+
+/// Trust Domain eXtensions sub-ioctl() commands
+#[repr(u32)]
+pub enum CmdId {
+    InitVcpu = 2,
+}
+
+/// Contains information for the sub-ioctl() command to be run. This is
+/// equivalent to `struct kvm_tdx_cmd` in the kernel.
+#[derive(Default)]
+#[repr(C)]
+pub struct Cmd {
+    /// TDX command identifier
+    pub id: u32,
+
+    /// Flags for sub-command. If sub-command doesn't use it, set to zero.
+    pub flags: u32,
+
+    /// A u64 representing a generic pointer to the respective ioctl input.
+    /// This data is read differently according to the TDX ioctl identifier.
+    pub data: u64,
+
+    /// Auxiliary error code. The sub-command may return TDX SEAMCALL status
+    /// code in addition to -Exxx.
+    pub error: u64,
+
+    /// Reserved.
+    pub _unused: u64,
+}
--- src/vcpu/mod.rs
+++ src/vcpu/mod.rs
@@ -1,1 +1,29 @@
 // SPDX-License-Identifier: Apache-2.0
+
+mod linux;
+
+use kvm_bindings::*;
+use vmm_sys_util::*;
+
+use crate::vcpu::linux::ioctl::Cmd;
+
+vmm_sys_util::ioctl_iowr_nr!(KVM_MEMORY_ENCRYPT_OP, KVMIO, 0xba, std::os::raw::c_ulong);
+
+impl crate::vm::TdxVm {
+    pub fn init_vcpu(vcpu: &kvm_ioctls::VcpuFd, hob_addr: u64) -> Result<(), crate::vm::TdxError> {
+        let mut cmd = Cmd {
+            id: 2,
+            flags: 0,
+            data: hob_addr as *const u64 as _,
+            error: 0,
+            _unused: 0,
+        };
+        let ret = unsafe { ioctl::ioctl_with_mut_ptr(vcpu, KVM_MEMORY_ENCRYPT_OP(), &mut cmd) };
+        if ret < 0 {
+            // TODO: convert this into a TDX error
+            // Need to implement a method that allows us to create a TDX error based on an integer
+            // error code
+        }
+        Ok(())
+    }
+}
