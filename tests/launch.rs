1c35ad978d629ca7c82219d35f149b9bb73d2a75
Jake Correnti <jakecorrenti+github@proton.me>
2024-05-16 22:41:40 -0400

    creating vcpu fails in test
    Signed-off-by: Jake Correnti <jakecorrenti+github@proton.me>

--- tests/launch.rs
+++ tests/launch.rs
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use kvm_ioctls::Kvm;
-
+use kvm_bindings::*;
 use tdx::launch::TdxVm;
 
 #[test]
 fn launch() {
     let kvm_fd = Kvm::new().unwrap();
     let tdx_vm = TdxVm::new(&kvm_fd).unwrap();
+    let mut cap: kvm_enable_cap = Default::default();
+    cap.cap = KVM_CAP_MAX_VCPUS;
+    cap.flags = 0;
+    cap.args[0] = 100;
     let caps = tdx_vm.get_capabilities().unwrap();
     let _ = tdx_vm.init_vm(&kvm_fd, &caps).unwrap();
+    let vcpu = tdx_vm.fd.create_vcpu(0).unwrap();
 }
